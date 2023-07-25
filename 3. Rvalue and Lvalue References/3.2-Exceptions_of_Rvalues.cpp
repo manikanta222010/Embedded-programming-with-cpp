fab5fcb1b9bd010c6dbce79a475bd57d0310fe16
Manikanta Srinivas Yedla <51736184+manikanta222010@users.noreply.github.com>
2023-07-25 15:24:51 +0530

    Delete 3. Rvalue and Lvalue References directory

--- 3. Rvalue and Lvalue References/3.2-Exceptions_of_Rvalues.cpp
+++
@@ -1,4 +0,0 @@
-// Earlier, we stated that rvalues are on the right side of an assignment, and lvalues can be on the left side of an assignment. It is important to note that this is not always true
-
-const int five= 5;
-five= 6;
